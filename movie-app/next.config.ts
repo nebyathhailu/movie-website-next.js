9d4df54f99fdce48d28a1cf80c6a95306155d25a
NHA <nebyathhailu@gmail.com>
2025-08-25 07:01:36 +0300

    Setup project

---
+++ movie-app/next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  turbopack:{
+    root:'/home/student/assignment-webs/next-movie-app/movie-website-next.js/movie-app',
+  },
+};
+
+export default nextConfig;
